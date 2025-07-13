95d56c5d93f0b1a5e9d5463d7f7d868429a117de
Tiru <thiru.rishiharan@gmail.com>
2025-07-13 21:09:33 +0530

    237 1176

--- delete_node_in_a_linked_list_237/solution.go
+++ delete_node_in_a_linked_list_237/solution.go
@@ -5,9 +5,6 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// deleteNode removes the given node (not tail) in O(1) time.
 func deleteNode(node *ListNode) {
-	// node is guaranteed non-tail and non-nil by LeetCode.
-	node.Val = node.Next.Val
-	node.Next = node.Next.Next
+	*node = *node.Next
 }
--- diet_plan_performance_1176/solution.go
+++ diet_plan_performance_1176/solution.go
@@ -1,31 +1,26 @@
 package diet_plan_performance_1176
 
 func dietPlanPerformance(calories []int, k, lower, upper int) int {
-	if k <= 0 || k > len(calories) {
+	n := len(calories)
+	if k > n {
 		return 0
 	}
-
-	// first window
-	sum := 0
-	for i := 0; i < k; i++ {
+	
+	sum, points := 0, 0
+	
+	for i := 0; i < n; i++ {
 		sum += calories[i]
-	}
-
-	points := 0
-	if sum < lower {
-		points--
-	} else if sum > upper {
-		points++
-	}
-
-	// sliding the window
-	for i := k; i < len(calories); i++ {
-		sum += calories[i] - calories[i-k]
-		if sum < lower {
-			points--
-		} else if sum > upper {
-			points++
+		if i >= k {
+			sum -= calories[i-k]
+		}
+		if i >= k-1 {
+			if sum < lower {
+				points--
+			} else if sum > upper {
+				points++
+			}
 		}
 	}
+	
 	return points
 }
