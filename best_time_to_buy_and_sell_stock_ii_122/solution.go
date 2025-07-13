8d5985006fe80088a9bfa6b9107f57d512fb6acd
Tiru <thiru.rishiharan@gmail.com>
2025-07-13 16:35:31 +0530

    added len check in 122

--- best_time_to_buy_and_sell_stock_ii_122/solution.go
+++ best_time_to_buy_and_sell_stock_ii_122/solution.go
@@ -1,10 +1,14 @@
 package best_time_to_buy_and_sell_stock_ii_122
 
 func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+	
 	totalProfit := 0
-	for i := 1; i < len(prices); i++ {
-		if prices[i] > prices[i-1] {
-			totalProfit += prices[i] - prices[i-1] // Add profit if price increases
+	for i := range len(prices) - 1 {
+		if diff := prices[i+1] - prices[i]; diff > 0 {
+			totalProfit += diff
 		}
 	}
 	return totalProfit
